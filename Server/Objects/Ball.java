cfe392468f41b55fc3d002f12fa2fabc423d678d
Williamxu98 <xuwilliam01@gmail.com>
2016-11-12 04:10:05 -0500

    Created "Ball" object

---
+++ Server/Objects/Ball.java
@@ -0,0 +1,10 @@
+package Objects;
+
+public class Ball extends Object{
+
+	public Ball(int x, int y, int width, int height, double hSpeed, double vSpeed) {
+		super(x, y, width, height, Object.BALL, hSpeed, vSpeed);
+		// TODO Auto-generated constructor stub
+	}
+
+}
