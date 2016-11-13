6d3c902ec102fb1f80031c6855394270c5df775a
Williamxu98 <xuwilliam01@gmail.com>
2016-11-13 01:18:29 -0500

    Added a way to close program from terminal, updated jar

--- Server/Server/Main.java
+++ Server/Server/Main.java
@@ -1,10 +1,21 @@
 package Server;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		Thread thread = new Thread(new Server());
 		thread.start();
+		
+		Scanner scan = new Scanner(System.in);
+		while (true)
+		{
+			if (scan.next().equals("exit"))
+			{
+				System.exit(0);
+			}
+		}
 
 	}
 
